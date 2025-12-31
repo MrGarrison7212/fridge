9d40c02e7fe59622d9a1984c61245ce0c6a311b0
MrGarrison7212 <bokipfc1992@gmail.com>
2025-12-31 17:41:15 +0100

    refactoring - using Lombok - FridgeItem refactoring

--- src/main/java/com/bojan/fridge/persistence/model/FridgeItem.java
+++ src/main/java/com/bojan/fridge/persistence/model/FridgeItem.java
@@ -1,12 +1,19 @@
 package com.bojan.fridge.persistence.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
 
 @Entity
 @Table(name = "fridge_item")
+@Getter
+@Setter
+@NoArgsConstructor
 public class FridgeItem {
 
     @Id
@@ -34,25 +41,6 @@ public class FridgeItem {
 
     @Column(length = 1000)
     private String notes;
-
-    public FridgeItem(){}
-
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-    public FridgeCategory getCategory() { return category; }
-    public void setCategory(FridgeCategory category) { this.category = category; }
-    public int getQuantity() { return quantity; }
-    public void setQuantity(int quantity) { this.quantity = quantity; }
-    public String getUnit() { return unit; }
-    public void setUnit(String unit) { this.unit = unit;}
-    public LocalDate getStoredAt() { return storedAt; }
-    public void setStoredAt(LocalDate storedAt) { this.storedAt = storedAt; }
-    public LocalDate getBestBefore() { return bestBefore; }
-    public void setBestBefore(LocalDate bestBefore) { this.bestBefore = bestBefore; }
-    public String getNotes() { return notes; }
-    public void setNotes(String notes) { this.notes = notes; }
 
     @PrePersist
     void prePersist() {
