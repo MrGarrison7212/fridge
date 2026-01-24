6b0e6a1f915f14f69af8e5ae340fe491e2962254
MrGarrison7212 <bokipfc1992@gmail.com>
2026-01-24 18:36:42 +0100

    update - JwtService

--- src/main/java/com/bojan/fridge/security/JwtService.java
+++ src/main/java/com/bojan/fridge/security/JwtService.java
@@ -1,16 +1,53 @@
 package com.bojan.fridge.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
-import java.security.Key;
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.time.Instant;
 
 @Service
 public class JwtService {
 
     private static final String SECRET = " mysecretkeymysecretkeymysecretkeymysecretkey ";
 
-    private Key getSigningKey() {
+    private SecretKey getSigningKey() {
         return Keys.hmacShaKeyFor(SECRET.getBytes());
+    }
+
+    public String generateToken(String username) {
+        Instant now = Instant.now();
+        Instant expiry = now.plusSeconds(3600);
+
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(Date.from(now))
+                .expiration(Date.from(expiry))
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return getAllClaims(token).getSubject();
+    }
+
+    public boolean isTokenValid(String token){
+        try {
+            Claims claims = getAllClaims(token);
+            Date exp = claims.getExpiration();
+            return exp.after(new java.util.Date());
+        } catch (Exception e){
+            return false;
+        }
+    }
+    private Claims getAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
     }
 }
